ac328eec932a46d197c0a8f3c996be464a86600d
IkMasterJer <JCARRASCOH@isise.edu.pe>
2022-11-18 19:06:27 -0500

    v1

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador(){
+    alert("SELECCIONASTE TU MASCOTA")
+}
+
+let btnMascotaJugador = document.getElementById("btnmascota")
+btnMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
